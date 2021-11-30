12c3a71f8d4a7b9cd0714d3c143f50a897556654
Miladin Radovic <miladin.radovic@awesome7.com>
2021-11-30 20:50:32 +0100

    Solution added.

---
+++ Sinergija.XamARDemo/Sinergija.XamARDemo/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sinergija.XamARDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
